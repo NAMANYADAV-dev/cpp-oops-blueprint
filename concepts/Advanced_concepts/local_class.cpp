dc48deeb5991b7724b56e00f65b79c1b827f0202
Naman Yadav <naman2006yad@gmail.com>
2026-06-17 12:11:37 +0000

    study more concepts

---
+++ concepts/Advanced_concepts/local_class.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+
+void display() {
+
+    class Student {
+
+
+            public:
+
+                void show() {
+
+                    std:: cout << "What is your name...\n";
+
+                }
+
+                
+    };
+
+    Student s;
+    s.show();
+
+
+}
+
+
+int main () {
+
+        display();
+        return 0 ;
+
+}
